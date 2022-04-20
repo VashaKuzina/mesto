5004877663542a92ec6b4423ab1b8bdfebcb2786
Ksenia Kuzina <ku2in4ky@yandex.ru>
2022-04-20 15:23:14 +0300

    fix:поправить замечания первого ревью

--- scripts/script.js
+++ scripts/script.js
@@ -2,25 +2,21 @@ let profile = document.querySelector('.profile');
 let editButton = profile.querySelector('.profile__edit-button');
 let popup = document.querySelector('.popup');
 let coloseButton = popup.querySelector('.popup__close-button');
-let nameInput = popup.querySelector('.popup__input-text_name');
-let jobInput = popup.querySelector('.popup__input-text_job');
-let formElement =popup.querySelector('.popup__submit-button');
+let nameInput = popup.querySelector('.popup__input-text_type_name');
+let jobInput = popup.querySelector('.popup__input-text_type_job');
+let formElement =popup.querySelector('.popup__container');
 let nameProfile = profile.querySelector('.profile__name');
 let jobProfile = profile.querySelector('.profile__status');
 let likeButton = document.querySelector('.element__like-button');
-console.log(likeButton);
-
-
-editButton.addEventListener('click', showPopup);
 
 function showPopup() {
-  popup.classList.add('popup_opend');
+  popup.classList.add('popup_opened');
+  nameInput.value = nameProfile.textContent;
+  jobInput.value = jobProfile.textContent;
 }
 
-coloseButton.addEventListener('click', closePopup);
-
 function closePopup() {
-  popup.classList.remove('popup_opend');
+  popup.classList.remove('popup_opened');
 }
 
 function formSubmitHandler(evt) {
@@ -28,14 +24,13 @@ function formSubmitHandler(evt) {
   nameProfile.textContent = nameInput.value;
   jobProfile.textContent = jobInput.value;
   closePopup();
-  console.log('Ghbdtn');
 }
 
-formElement.addEventListener('click', formSubmitHandler); 
+editButton.addEventListener('click', showPopup);
 
-likeButton.addEventListener('click', likeActive);
+formElement.addEventListener('submit', formSubmitHandler);
 
-function likeActive() {
-  likeButton.classList.remove('element__like-button_disabled');
-  likeButton.classList.add('element__like-button_active');
-}
+coloseButton.addEventListener('click', closePopup);
+
+
+
