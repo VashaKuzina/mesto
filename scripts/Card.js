fceeb958f5925022ab945419357ece0728dcaec2
Ksenia Kuzina <ku2in4ky@yandex.ru>
2022-09-02 00:24:13 +0300

    fix: объявить дополнительные классовые элементы вместо поиска несколько раз

--- scripts/Card.js
+++ scripts/Card.js
@@ -22,30 +22,33 @@ export default class Card{
     generateCard() {
       this._element = this._getTemplate();
       this._setEventListeners();
-  
       this._element.querySelector('.element__name').textContent = this._name;
-      this._element.querySelector('.element__image').src = this._link;
-      this._element.querySelector('.element__name').textContent = this._name;
+      this._image.src = this._link;
 
       return this._element;
     }
 
     _setEventListeners() {
-      this._element.querySelector('.element__like-button').addEventListener('click', () => {
+
+      this._image = this._element.querySelector('.element__image');
+      this._likeButton = this._element.querySelector('.element__like-button');
+      this._deleteButton =this._element.querySelector('.element__delete-button');
+
+      this._likeButton.addEventListener('click', () => {
         this._handleLikeClick();
       });
 
-      this._element.querySelector('.element__delete-button').addEventListener('click', () => {
+      this._deleteButton.addEventListener('click', () => {
         this._handleRemoveClick();
       });
 
-      this._element.querySelector('.element__image').addEventListener('click', () => {
+      this._image.addEventListener('click', () => {
         this._handlePopupViewClick();
       });
     }
 
     _handleLikeClick(){
-      this._element.querySelector('.element__like-button').classList.toggle('element__like-button_active');
+      this._likeButton.classList.toggle('element__like-button_active');
     }
     
     _handleRemoveClick(){
